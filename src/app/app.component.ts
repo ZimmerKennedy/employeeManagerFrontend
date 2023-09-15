064eacb14677a38efcabea39a9c9073cfdc76531
Zimmer Kennedy <kennedyzimmer00@gmail.com>
2023-09-15 13:13:34 +0300

    Frontend-Employee-Manager

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'employee_manager_app';
+  public employees!: Employee[];
+
+  constructor(private employeeService: EmployeeService){}
+  
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+    )
+  };
 }
